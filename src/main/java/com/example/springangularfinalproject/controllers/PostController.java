68f87b9974373e9b7843a2e1f1c13caf16a0d784
Kristjan Gauk <kristjangauk@gmail.com>
2021-11-21 21:39:21 +0200

    teine commit
    lisamine ja muutmine ei tööta veel, mingi kamm on ei saa aru

--- src/main/java/com/example/springangularfinalproject/controllers/PostController.java
+++ src/main/java/com/example/springangularfinalproject/controllers/PostController.java
@@ -4,29 +4,51 @@ import com.example.springangularfinalproject.entities.Post;
 import com.example.springangularfinalproject.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
 
 @Controller
-@RequestMapping("/api")
-@ResponseBody
 public class PostController {
 
     @Autowired
     private PostRepository postRepository;
 
-    @GetMapping("/posts")
-    public List<Post> getPosts() {
-        return postRepository.findAll();
+
+    @GetMapping("/list")
+    public ModelAndView getAllPosts() {
+        ModelAndView mav = new ModelAndView("list-posts");
+        mav.addObject("posts", postRepository.findAll());
+        return mav;
     }
 
-    @PostMapping("/post")
-    public Post createTodo(@Validated @RequestBody Post post) {
-        Post createdPost = new Post();
-        createdPost.setPost(post.getPost());
-        createdPost.setUserId(post.getUserId());
-        return postRepository.save(post);
+    @GetMapping("/addPostForm")
+    public ModelAndView addPostForm() {
+        ModelAndView mav = new ModelAndView("add-post-form");
+        Post newPost = new Post();
+        mav.addObject("post", newPost);
+        return mav;
     }
+
+    @PostMapping("/savePost")
+    public String savePost(@ModelAttribute Post post) {
+        postRepository.save(post);
+        return "redirect:/list";
+    }
+
+    @GetMapping("/showUpdateForm")
+    public ModelAndView showUpdateForm(@RequestParam Long postId) {
+        ModelAndView mav = new ModelAndView("add-post-form");
+        Post post = postRepository.findById(postId).get();
+        mav.addObject("post", post);
+        return mav;
+    }
+
+    @GetMapping("/deletePost")
+    public String deletePost(@RequestParam Long postId) {
+        postRepository.deleteById(postId);
+        return "redirect:/list";
+    }
+
+
 }
